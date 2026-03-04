9bcbb3fc3f63b239b97704e2f5c953f217931eba
Hohlyandiya <teroryk@gmail.com>
2026-03-04 16:22:26 +0300

    change eslint.config.js and vite.config.js

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,9 +1,5 @@
-import js from '@eslint/js'
 import globals from 'globals'
-import reactPlugin  from "eslint-plugin-react";
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import { defineConfig } from 'eslint/config'
 import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig([
@@ -26,7 +22,7 @@ export default defineConfig([
     },
     settings: {
       react: {
-        version: "detect"
+        version: 'detect'
       }
     },
     rules: {
