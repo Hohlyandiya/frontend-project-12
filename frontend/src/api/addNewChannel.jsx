bf6879785cfc47c59603ca84960bd7433c604f1a
Hohlyandiya <teroryk@gmail.com>
2026-04-09 14:53:28 +0300

    fix change channel

--- frontend/src/api/addNewChannel.jsx
+++ frontend/src/api/addNewChannel.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getApi } from "./apiInstance";
 
-const addNewChannel = (newChannel/* , token */) => {
+const addNewChannel = (newChannel/* , token */, setCurrentChannel) => {
   axios.post('/api/v1/channels', newChannel, getApi()/* {
   headers: {
     Authorization: `Bearer ${token}`,
