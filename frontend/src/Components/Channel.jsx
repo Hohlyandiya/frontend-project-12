ecd97e3bdb3e7eb1835aaedb945c6f7ea54936f0
Hohlyandiya <teroryk@gmail.com>
2026-04-10 12:14:01 +0300

    fix syntax

--- frontend/src/Components/Channel.jsx
+++ frontend/src/Components/Channel.jsx
@@ -3,27 +3,29 @@ import ButtonChannel from './UI/ButtonChannel'
 
 const Channel = ({ channel, currentChannel, setCurrentChannel, setAction, setShow, setSelectedChannel }) => {
   return channel.removable
-    ? (
-      <li key={channel.id} className="nav-item w-100">
-        <DropDownToggle
-          channel={channel}
-          currentChannel={currentChannel}
-          setCurrentChannel={setCurrentChannel}
-          setAction={setAction}
-          setShow={setShow}
-          setSelectedChannel={setSelectedChannel}
-        />
-      </li>
-    )
-    : (
-      <li key={channel.id} className="nav-item w-100">
-        <ButtonChannel
-          channel={channel}
-          currentChannel={currentChannel}
-          setCurrentChannel={setCurrentChannel}
-        />
-      </li>
-    )
+    ?
+      (
+        <li key={channel.id} className="nav-item w-100">
+          <DropDownToggle
+            channel={channel}
+            currentChannel={currentChannel}
+            setCurrentChannel={setCurrentChannel}
+            setAction={setAction}
+            setShow={setShow}
+            setSelectedChannel={setSelectedChannel}
+          />
+        </li>
+      )
+    :
+      (
+        <li key={channel.id} className="nav-item w-100">
+          <ButtonChannel
+            channel={channel}
+            currentChannel={currentChannel}
+            setCurrentChannel={setCurrentChannel}
+          />
+        </li>
+      )
 }
 
 export default Channel
